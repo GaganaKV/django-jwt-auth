84ce3d33a5e7c2097ac1d89511cba26a5f60217a
Gagana K V <gaganakv2018@gmail.com>
2025-11-12 12:15:08 +0530

    Cleaned and fixed Django settings for deployment

--- jwt_auth_project/settings.py
+++ jwt_auth_project/settings.py
@@ -15,7 +15,7 @@ import os
 from dotenv import load_dotenv
 
 from datetime import timedelta
-
+import dj_database_url
 # Load environment variables from .env file
 load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -99,12 +100,9 @@ WSGI_APPLICATION = 'jwt_auth_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+# Database configuration (Render or local SQLite fallback)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
 }
 
 
@@ -142,9 +140,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-
+# Static files
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'users', 'static')]
+
+
+# Use WhiteNoise for serving static files efficiently
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
